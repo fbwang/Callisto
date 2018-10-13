c71a638ddcbcaf4904ab7c310090e0a3639b6617
fbwang <crystal19910327@gmail.com>
2018-10-13 17:01:53 +0800

    Add Template Component

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <!--<landing-page></landing-page>-->
     <template-file></template-file>
+    <!--<data-file></data-file>-->
   </div>
 </template>
 
@@ -9,9 +10,11 @@
   // import LandingPage from "./components/LandingPage";
 
   import TemplateFile from "./components/TemplateFile";
+  // import DataFile from "./components/TemplateFile/DataFile";
   export default {
     name: 'callisto',
     components: {
+        // DataFile,
         TemplateFile
         // LandingPage,
     }
