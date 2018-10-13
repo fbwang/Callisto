6dccc212edfb2e5e05d5add8f6d157ae481fa062
fbwang <crystal19910327@gmail.com>
2018-10-13 16:18:48 +0800

    Init Callisto

---
+++ src/renderer/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <!--<landing-page></landing-page>-->
+    <template-file></template-file>
+  </div>
+</template>
+
+<script>
+  // import LandingPage from "./components/LandingPage";
+
+  import TemplateFile from "./components/TemplateFile";
+  export default {
+    name: 'callisto',
+    components: {
+        TemplateFile
+        // LandingPage,
+    }
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
---
+++ src/renderer/components/TemplateFile.vue
@@ -0,0 +1,24 @@
+<template>
+    <main>
+    <div>
+        Drag Your File Here
+    </div>
+    <div id="frame">
+
+    </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: "TemplateFile"
+    }
+</script>
+
+<style scoped>
+#frame{
+    height: 300px;
+    width: 400px;
+    border: dashed 1px;
+}
+</style>
