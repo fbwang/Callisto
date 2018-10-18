00c6eb5938ab3ea95780e94a6b3cefc0a8387947
unknown <fbwang@DESKTOP-47G4B8Q.amd.com>
2018-10-18 17:43:14 +0800

    Add method in DataFile.vue

--- src/renderer/components/TemplateFile/DataFile.vue
+++ src/renderer/components/TemplateFile/DataFile.vue
@@ -3,15 +3,20 @@
     <div align="center">
         Drag Your Data File Folder Here
     </div>
-    <div class="frame" id="test">
-        <form action="/upload" class="dropzone"></form>
+    <div class="frame" id="folder" v-on:drop="diropen" v-on:dragover="dirover">
+            <div align="center">Datas: </div>
+            <span>{{folder}}</span>
+        </div>
+    <div align="center">
+        <button v-on:click="execute">Submit</button>
     </div>
     </div>
 </template>
 
 <script>
-    import Dropzone from 'dropzone';
+    // import Dropzone from 'dropzone';
     // var myDropzone = new Dropzone("div#files", { url: "/file/post"});
+    import Excel from 'exceljs'
 
     export default {
         name: "DataFile",
@@ -21,17 +26,31 @@
             }
         },
         methods: {
-            fopen: function (e) {
-                e.preventDefault();
-                e.stopPropagation();
-                for (let f of event.dataTransfer.files) {
-                    this.folder = f.path;
-                    console.log('File(s) you dragged here: ', f.path);
-                }
-            },
-            fover:function (event) {
+            diropen:function (event) {
                 event.preventDefault();
                 event.stopPropagation();
+
+                for (let f of event.dataTransfer.files) {
+                    this.folder = f.path;
+                    console.log('Folder you dragged here: ', f.path);
+                }
+            },
+            dirover:function (event) {
+                event.preventDefault();
+                event.stopPropagation();
+            },
+
+            execute:function(event){
+                // console.log(Excel)
+                var workbook = new Excel.Workbook();
+                // console.log(workbook)
+                workbook.xlsx.readFile('Frametime_Analysis_Template_10-10-2018.xlsx').then(function(){
+                    var worksheet = workbook.getWorksheet('Data');
+                    console.log(worksheet);
+                    var items = worksheet.getColumn('O');
+                    console.log (items);
+
+                })
             }
         }
     };
@@ -40,6 +59,6 @@
 </script>
 
 <style>
-    @import '~dropzone/dist/dropzone.css';
+    /* @import '~dropzone/dist/dropzone.css'; */
 
 </style>
